080ce88af215a6e0f3f8461dc0f3cdd45e9c3ecd
Jrenatomt <jrenatomt@gmail.com>
2020-06-15 02:35:59 -0300

    Projeto Finalizado

--- src/main/java/com/renato/controleFinancas/ControleFinancasApplication.java
+++ src/main/java/com/renato/controleFinancas/ControleFinancasApplication.java
@@ -2,9 +2,6 @@ package com.renato.controleFinancas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ControleFinancasApplication {
